4d9a7e2c325a072a675040244f0cba75aef2aaba
pedroagaeme <futurograduado@gmail.com>
2024-07-26 22:33:42 -0300

    Ajuste das questões 5 e 6

--- Exercício 5/SimuladorOnibus.java
+++ Exercício 5/SimuladorOnibus.java
@@ -3,66 +3,6 @@ import java.util.Collections;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.locks.ReentrantLock;
 
-class Parada {
-    private final int CAPACIDADE = 50;
-    private int numeroPassageirosNoOnibus = 0;
-    private int passageirosPrevistos = 100;
-    private ArrayList<Passageiro> passageirosEsperando = new ArrayList<>();
-    private ArrayList<Passageiro> passageirosNoOnibus = new ArrayList<>();
-    private final ReentrantLock lock = new ReentrantLock();
-    private int contador = 0;
-
-    public void chegarParada(Onibus onibus) throws InterruptedException {
-        while (passageirosPrevistos > 0) {
-            lock.lock();
-            try {
-                System.out.println("Onibus chegou na parada.");
-                numeroPassageirosNoOnibus = 0;
-                Collections.shuffle(passageirosEsperando); 
-                while (numeroPassageirosNoOnibus < CAPACIDADE && !passageirosEsperando.isEmpty()) {
-                    Passageiro passageiro = passageirosEsperando.get(0);
-                    numeroPassageirosNoOnibus++;
-                    passageirosEsperando.remove(0);
-                    passageirosPrevistos--;
-                    passageirosNoOnibus.add(passageiro);
-                }
-            } finally {
-                lock.unlock();
-            }
-            while (!passageirosNoOnibus.isEmpty()) {
-                Passageiro passageiro = passageirosNoOnibus.get(0);
-                passageirosNoOnibus.remove(0);
-                contador++;
-                System.out.println(passageiro.getNome() + " entrou no onibus. Lugares Ocupados: " + contador);
-                Thread.sleep((long) (Math.random() * 10));
-            }
-            contador = 0;
-            lock.lock();
-            try {
-                if (numeroPassageirosNoOnibus > 0) {
-                    System.out.println("Onibus partiu com " + numeroPassageirosNoOnibus + " passageiros.");
-                } else {
-                    System.out.println("Onibus partiu vazio.");
-                }
-            } finally {
-                lock.unlock();
-            }
-            Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 3000));
-        }
-    }
-
-    public void entrarOnibus(Passageiro passageiro) throws InterruptedException {
-        Thread.sleep((long) (Math.random() * 10000));
-        lock.lock();
-        try {
-            System.out.println(passageiro.getNome() + " chegou na parada.");
-            passageirosEsperando.add(passageiro);
-        } finally {
-            lock.unlock();
-        }
-    }
-}
-
 class Onibus extends Thread {
     private Parada parada;
 
@@ -90,21 +30,81 @@ class Passageiro extends Thread {
     }
 
     public String getNome() {
-        return nome;
+        return this.nome;
     }
 
     @Override
     public void run() {
         try {
-            parada.entrarOnibus(this);
+            this.parada.entrarOnibus(this);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 }
 
+class Parada {
+    private final int CAPACIDADE = 50;
+    private int numeroPassageirosNoOnibus = 0;
+    private int passageirosPrevistos = 100;
+    private ArrayList<Passageiro> passageirosEsperando = new ArrayList<>();
+    private ArrayList<Passageiro> passageirosNoOnibus = new ArrayList<>();
+    private final ReentrantLock lock = new ReentrantLock();
+    private int contador = 0;
+
+    public void chegarParada(Onibus onibus) throws InterruptedException {
+        while (this.passageirosPrevistos > 0) {
+            this.lock.lock();
+            try {
+                System.out.println("Onibus chegou na parada.");
+                this.numeroPassageirosNoOnibus = 0;
+                Collections.shuffle(this.passageirosEsperando); 
+                while (this.numeroPassageirosNoOnibus < this.CAPACIDADE && !this.passageirosEsperando.isEmpty()) {
+                    Passageiro passageiro = this.passageirosEsperando.get(0);
+                    this.numeroPassageirosNoOnibus++;
+                    this.passageirosEsperando.remove(0);
+                    this.passageirosPrevistos--;
+                    this.passageirosNoOnibus.add(passageiro);
+                }
+            } finally {
+                this.lock.unlock();
+            }
+            while (!this.passageirosNoOnibus.isEmpty()) {
+                Passageiro passageiro = this.passageirosNoOnibus.get(0);
+                this.passageirosNoOnibus.remove(0);
+                this.contador++;
+                System.out.println(passageiro.getNome() + " entrou no onibus. Lugares Ocupados: " + this.contador);
+                Thread.sleep((long) (Math.random() * 10));
+            }
+            this.contador = 0;
+            this.lock.lock();
+            try {
+                if (this.numeroPassageirosNoOnibus > 0) {
+                    System.out.println("Onibus partiu com " + this.numeroPassageirosNoOnibus + " passageiros.");
+                } else {
+                    System.out.println("Onibus partiu vazio.");
+                }
+            } finally {
+                this.lock.unlock();
+            }
+            Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 3000));
+        }
+    }
+
+    public void entrarOnibus(Passageiro passageiro) throws InterruptedException {
+        Thread.sleep((long) (Math.random() * 10000));
+        this.lock.lock();
+        try {
+            System.out.println(passageiro.getNome() + " chegou na parada.");
+            this.passageirosEsperando.add(passageiro);
+        } finally {
+            this.lock.unlock();
+        }
+    }
+}
+
 public class SimuladorOnibus {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Parada parada = new Parada();
 
         for (int i = 1; i <= 100; i++) {
--- Exercício 6/SimuladorBanheiro.java
+++ Exercício 6/SimuladorBanheiro.java
@@ -2,118 +2,6 @@ import java.util.ArrayList;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
-class Banheiro {
-    private final int CAPACIDADE = 3;
-    private final ReentrantLock lock = new ReentrantLock(true);
-    private final Condition vezHomens = lock.newCondition();
-    private final Condition vezMulheres = lock.newCondition();
-    private final Condition alguemChegou = lock.newCondition();
-    private final Condition esperarVez = lock.newCondition();
-    private int homensNoBanheiro = 0;
-    private int mulheresNoBanheiro = 0;
-    private int pessoasPrevistas = 100;
-    private ArrayList<Pessoa>  fila = new ArrayList<>();
-
-
-    public void controlarEntrada() throws InterruptedException {
-        while(pessoasPrevistas > 0) {
-        this.lock.lock();
-        try {
-            if (!fila.isEmpty()) {
-                if(fila.get(0).checarGenero()) {
-                    while (mulheresNoBanheiro > 0) {
-                        vezHomens.await();
-                    }
-                    if(homensNoBanheiro < CAPACIDADE) {
-                        homensNoBanheiro++;
-                        System.out.println(fila.get(0).checarNome() + " entrou no banheiro. Homens: " + homensNoBanheiro);
-                        pessoasPrevistas--;
-                        fila.get(0).garantirLugar();
-                        esperarVez.signalAll();
-                        fila.remove(0);
-                    }
-                }
-                else {
-                    while(homensNoBanheiro > 0) {
-                        vezMulheres.await();
-                    }
-                    if(mulheresNoBanheiro < CAPACIDADE) {
-                        mulheresNoBanheiro++;
-                        System.out.println(fila.get(0).checarNome() + " entrou no banheiro. Mulheres: " + mulheresNoBanheiro);
-                        pessoasPrevistas--;
-                        fila.get(0).garantirLugar();
-                        esperarVez.signalAll();
-                        fila.remove(0);
-                    }
-                }
-            }
-            else {
-                while(fila.isEmpty()) {
-                    alguemChegou.await();
-                }
-            }
-        } finally {
-            lock.unlock();
-        }
-        }
-    }
-    public void entrar(Pessoa pessoa) throws InterruptedException {
-        this.lock.lock();
-        try {
-            System.out.println(pessoa.checarNome() + " quer usar o banheiro.");
-            fila.add(pessoa);
-            if(fila.size() == 1) {
-                alguemChegou.signalAll();
-            }
-            while(!pessoa.checarLugar()) {
-                esperarVez.await();
-            }
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    public void sair(Pessoa pessoa) throws InterruptedException {
-        Thread.sleep((long) (Math.random() * 1000)); 
-        this.lock.lock();
-        try {
-            if(pessoa.checarGenero()) {
-                homensNoBanheiro--;
-                System.out.println(pessoa.checarNome() + " saiu do banheiro. Homens restantes: " + homensNoBanheiro);
-                if(homensNoBanheiro == 0) {
-                    vezMulheres.signalAll();
-                }
-            }
-            else {
-                mulheresNoBanheiro--;
-                System.out.println(pessoa.checarNome() + " saiu do banheiro. Mulheres restantes: " + mulheresNoBanheiro);
-                if(mulheresNoBanheiro == 0) {
-                    vezHomens.signalAll();
-                }
-            }
-        } finally {
-            lock.unlock();
-        }
-    }
-}
-
-class Controle extends Thread {
-    private Banheiro banheiro;
-
-    public Controle(Banheiro banheiro) {
-        this.banheiro = banheiro;
-    }
-
-    @Override
-    public void run() {
-        try {
-            this.banheiro.controlarEntrada();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
 class Pessoa extends Thread {
     private String nome;
     private boolean isHomem;
@@ -151,6 +39,119 @@ class Pessoa extends Thread {
             this.banheiro.sair(this);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+}
+
+class Controle extends Thread {
+    private Banheiro banheiro;
+
+    public Controle(Banheiro banheiro) {
+        this.banheiro = banheiro;
+    }
+
+    @Override
+    public void run() {
+        try {
+            this.banheiro.controlarEntrada();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+class Banheiro {
+    private final int CAPACIDADE = 3;
+    private final ReentrantLock lock = new ReentrantLock(true);
+    private final Condition vezHomens = lock.newCondition();
+    private final Condition vezMulheres = lock.newCondition();
+    private final Condition alguemChegou = lock.newCondition();
+    private final Condition esperarVez = lock.newCondition();
+    private int homensNoBanheiro = 0;
+    private int mulheresNoBanheiro = 0;
+    private int pessoasPrevistas = 100;
+    private ArrayList<Pessoa>  fila = new ArrayList<>();
+
+
+    public void controlarEntrada() throws InterruptedException {
+        while(this.pessoasPrevistas > 0) {
+        this.lock.lock();
+        try {
+            if (!this.fila.isEmpty()) {
+                if(this.fila.get(0).checarGenero()) {
+                    while (this.mulheresNoBanheiro > 0) {
+                        this.vezHomens.await();
+                    }
+                    if(this.homensNoBanheiro < this.CAPACIDADE) {
+                        this.homensNoBanheiro++;
+                        System.out.println(this.fila.get(0).checarNome() + " entrou no banheiro. Homens: " + this.homensNoBanheiro);
+                        this.pessoasPrevistas--;
+                        this.fila.get(0).garantirLugar();
+                        this.esperarVez.signalAll();
+                        this.fila.remove(0);
+                    }
+                }
+                else {
+                    while(this.homensNoBanheiro > 0) {
+                        this.vezMulheres.await();
+                    }
+                    if(this.mulheresNoBanheiro < this.CAPACIDADE) {
+                        this.mulheresNoBanheiro++;
+                        System.out.println(this.fila.get(0).checarNome() + " entrou no banheiro. Mulheres: " + this.mulheresNoBanheiro);
+                        this.pessoasPrevistas--;
+                        this.fila.get(0).garantirLugar();
+                        this.esperarVez.signalAll();
+                        this.fila.remove(0);
+                    }
+                }
+            }
+            else {
+                while(this.fila.isEmpty()) {
+                    this.alguemChegou.await();
+                }
+            }
+        } finally {
+            this.lock.unlock();
+        }
+        }
+    }
+    public void entrar(Pessoa pessoa) throws InterruptedException {
+        this.lock.lock();
+        try {
+            System.out.println(pessoa.checarNome() + " quer usar o banheiro.");
+            this.fila.add(pessoa);
+            if(this.fila.size() == 1) {
+                this.alguemChegou.signalAll();
+            }
+            while(!pessoa.checarLugar()) {
+                this.esperarVez.await();
+            }
+        } finally {
+            this.lock.unlock();
+        }
+    }
+
+    public void sair(Pessoa pessoa) throws InterruptedException {
+        Thread.sleep((long) (Math.random() * 1000)); 
+        this.lock.lock();
+        try {
+            if(pessoa.checarGenero()) {
+                this.homensNoBanheiro--;
+                System.out.println(pessoa.checarNome() + " saiu do banheiro. Homens restantes: " + this.homensNoBanheiro);
+                if(this.homensNoBanheiro == 0) {
+                    this.vezMulheres.signalAll();
+                }
+            }
+            else {
+                this.mulheresNoBanheiro--;
+                System.out.println(pessoa.checarNome() + " saiu do banheiro. Mulheres restantes: " + this.mulheresNoBanheiro);
+                if(this.mulheresNoBanheiro == 0) {
+                    vezHomens.signalAll();
+                }
+            }
+        } finally {
+            this.lock.unlock();
         }
     }
 }
